7d6f764ff0b8393ba21bc56e00ff9d172138ef44
Caio Bomfim <caaiobomfim@gmail.com>
2025-08-02 10:51:09 -0300

    first commit

---
+++ src/main/java/dev/journey/restcountries/JourneyLabRestCountriesApplication.java
@@ -0,0 +1,13 @@
+package dev.journey.restcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyLabRestCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyLabRestCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/journey/restcountries/JourneyLabRestCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.journey.restcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyLabRestCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
